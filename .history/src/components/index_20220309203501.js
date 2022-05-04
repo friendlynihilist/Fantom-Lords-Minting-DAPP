55c24b9fce6f33138771fe13adde0b055b527628
Carlo Teo Pedretti <alphacentauri90@gmail.com>
2022-05-05 00:05:58 +0200

    pre web3 update

--- .history/src/components/index_20220309203501.js
+++
@@ -1,5 +0,0 @@
-export { default as Navigation } from "./Navigation";
-export { default as Footer } from "./Footer";
-export { default as Army } from "./Army";
-export { default as Stronghold } from "./Stronghold";
-export { default as Lore } from "./Lore";
---
+++ .history/src/index_20220427224243.js
@@ -0,0 +1,40 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  Navigation,
+  Footer,
+  Army,
+  Stronghold,
+  Lore,
+  Runesmith
+} from "./components";
+import reportWebVitals from "./reportWebVitals";
+import store from "./redux/store";
+import { Provider } from "react-redux";
+import "./styles/reset.css";
+import "./styles/resp.css";
+
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+    <Navigation />
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/army" element={<Army />} />
+      <Route path="/lore" element={<Lore />} />
+      <Route path="/stronghold" element={<Stronghold />} />
+      <Route path="/runesmith" element={<Runesmith />} />
+    </Routes>
+    <Footer />
+    </Router>
+  </Provider>,
+  document.getElementById("root")
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
